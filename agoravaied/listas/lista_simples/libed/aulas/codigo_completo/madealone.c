fc23a9fa3cf24f9a717989a00d7778c087e818a0
Júlio César <juliocesar.gm6000@gmail.com>
2024-03-29 17:24:54 -0300

    adding the final files about linked lists

--- agoravaied/listas/lista_simples/libed/aulas/codigo_completo/madealone.c
+++ agoravaied/listas/lista_simples/libed/aulas/codigo_completo/madealone.c
@@ -154,20 +154,161 @@ void list_removeNode(LinkedList *L, int value)
         prev->next = pos->next; // atualiza o prev, agora podemos remover o nó pos sem problemas, não perdemos referencia.
     if (pos->next == NULL)
         L->end = prev;
-
+    L->size--;
     free(pos);
 }
 
-void list_destroy(LinkedList* L){
-    SNode* remove = NULL;
-    SNode* p = L->begin;
+void list_destroy(LinkedList *L)
+{
+    SNode *remove = NULL;
+    SNode *p = L->begin;
 
-    while(p != NULL){
+    while (p != NULL)
+    {
         remove = p;
         p = p->next;
         free(remove);
     }
     free(L);
+}
+// inserir um elemento na posição que o usuario escolher
+void list_insertElementInUserChoose(LinkedList *L, int index, int value)
+{
+    if (list_isEmpty(L))
+    {
+        printf("List is empty\n");
+        return;
+    }
+    if (index < 0 || index > list_size(L))
+    {
+        printf("Invalid Index: %d\n", index);
+        return;
+    }
+
+    if (index == 0)
+    {
+        list_addFirst(L, value);
+        return;
+    }
+    if (index == L->size)
+    {
+        list_addLast(L, value);
+        return;
+    }
+
+    SNode *p = L->begin;
+    SNode *aux = NULL;
+    SNode *newnode = SNode_create(value);
+    int i = 0;
+
+    while (index != i)
+    {
+        aux = p;
+        p = p->next;
+        i++;
+    }
+    aux->next = newnode;
+    newnode->next = p;
+    L->size++;
+}
+
+void list_reverse(LinkedList *L)
+{
+    LinkedList *aux = list_create();
+    SNode *pL = L->begin;
+
+    for (int i = L->size - 1; i >= 0; i--)
+    {
+        list_addFirst(aux, pL->val);
+        pL = pL->next;
+    }
+
+    L->begin = aux->begin;
+    L->end = aux->end;
+    L->size = aux->size;
+
+    free(aux);
+}
+
+LinkedList *list_clone(const LinkedList *L)
+{
+    LinkedList *clone = list_create();
+    SNode *current = L->begin;
+
+    while (current != NULL)
+    {
+        list_addLast(clone, current->val);
+        current = current->next;
+    }
+    return clone;
+}
+
+void list_removeAllNodes(LinkedList *L)
+{
+    SNode *current = L->begin; // Comece a partir do início da lista
+
+    // Percorra os nós da lista e remova cada nó usando list_removeNode
+    while (current != NULL)
+    {
+        SNode *temp = current;         // Salve uma referência ao nó atual
+        current = current->next;       // Avance para o próximo nó
+        list_removeNode(L, temp->val); // Remova o nó atual usando list_removeNode
+    }
+}
+
+void list_concatenate(LinkedList *L1, LinkedList *L2)
+{
+    if (list_isEmpty(L1) || list_isEmpty(L2))
+    {
+        printf("Cannot concatenate empty lists\n");
+        return;
+    }
+
+    SNode *p = L1->end;
+    p = L2->begin;
+    L1->end = L2->end;
+    L1->size += L2->size;
+
+    list_destroy(L2);
+}
+
+void list_insertOrdered(LinkedList *L, int value)
+{
+    SNode *newNode = SNode_create(value);
+
+    if (list_isEmpty(L) || value < L->begin->val)
+    {
+        list_addFirst(L, value);
+        return;
+    }
+    SNode *prev = NULL;
+    SNode *pos = L->begin;
+    while (pos != NULL && value > pos->val)
+    {
+        prev = pos;
+        pos = pos->next;
+    }
+    newNode->next = pos;
+    prev->next = newNode;
+
+    if (pos == NULL)
+        L->end = pos;
+
+    L->size++;
+}
+
+void list_sort(LinkedList *L){
+    if(L->size <= 1)
+        return;
+
+    LinkedList *aux = list_create();
+    for(SNode *p = L->begin; p != NULL; p = p->next)
+        list_insertOrdered(aux, p->val);
+
+    list_destroy(L);
+    L->begin = aux->begin;
+    L->end = aux->end;
+    free(aux);
 }
 
 void main()
@@ -177,10 +318,16 @@ void main()
     list_addFirst(L, 12);
     list_addLast(L, 14);
     list_addFirst(L, 21);
-    list_removeNode(L, 12);
     list_addLast(L, 45);
 
-    // list_userValue(L, -12);
+    list_removeNode(L, 12);
+
+    list_insertElementInUserChoose(L, 1, 3);
+
+    list_reverse(L);
+    // list_userValue(L, 2);
+
+    list_sort(L);
 
     list_print(L);
 }
