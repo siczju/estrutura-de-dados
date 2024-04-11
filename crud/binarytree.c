10b5cb0bc6b739528ab3dc0f36024ea9cc808cef
Júlio César <juliocesar.gm6000@gmail.com>
2024-04-10 21:48:26 -0300

    starting the binary tree crud

--- crud/binarytree.c
+++ crud/binarytree.c
@@ -2,13 +2,39 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct no
+typedef struct _node
 {
-    int conteudo; // valor
-    struct no *direita, *esquerda;
-} No;
+    int val; // valor
+    struct no *right, *left;
+} Node;
 
-typedef struct
+typedef struct _binarytree
 {
-    No *raiz;
+    No *root;
 } ArvB;
+
+ArvB* tree_create(){
+    ArvB *newTree = (ArvB*) calloc(1, sizeof(ArvB));
+    newTree->root = NULL;
+
+    return newTree;
+}
+
+Node* node_create(int val){
+    Node *newNode = (Node*) calloc(1, sizeof(Node));
+    newNode->right = NULL;
+    newNode->left = NULL;
+
+    return newNode;
+}
+void insert(Node *newNode, int val){
+
+    if(newNode == NULL)
+        return node_create(val);
+    else{
+        if(val < newNode->val)
+    }
+
+}
+
+
